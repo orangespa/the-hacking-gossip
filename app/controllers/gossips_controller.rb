cd26222c1aedc920723a53a0fc8ab2f0a3711a7f
hong <luu.bichhong@gmail.com>
2017-07-28 10:08:54 +0200

    show controller

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,7 +1,28 @@
 class GossipsController < ApplicationController
 
   def index
-    @gossips = Gossip.all
+    @gossip = Gossip.all
   end
 
+  def new
+    @gossip = Gossip.new
+  end
+
+   def create
+      @gossip = Gossip.new(gossip_params)
+      if @gossip.save
+        redirect_to @gossip 
+      else
+        puts "oups"
+      end
+    end
+
+  def show
+    @gossip = Gossip.find(params[:id])
+  end
+  
+    private
+      def gossip_params
+        params.require(:gossip).permit(:anonymous_author, :content)
+      end
 end
--- app/models/gossip.rb
+++ app/models/gossip.rb
@@ -1,4 +1,3 @@
 class Gossip < ApplicationRecord
-  has_many: @gossip
-  <%= form_for @anonymous.each 
+  
 end
